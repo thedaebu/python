543bf994b2390be0c99b2754618f60bace7d94cb
thedaebu <min.kim163@gmail.com>
2021-07-05 14:24:04 -0400

    first commit

---
+++ python_practice.py
@@ -0,0 +1,42 @@
+# newnum = "Hello {1} {0}".format("my", "dude.")
+# print(newnum)
+
+# i = 0
+# while i < 5:
+#     print(i)
+#     i += 1
+
+# def divide(a,b):
+#     return a%b
+
+# print(divide(1,2))
+
+# str = ["what's", "up"]
+# print(" ".join(str))
+
+# print("{:,}".format(1234))
+
+# print("Percentage is {:.2%}".format(1/2))
+
+# print("hello")
+# answer = input("how are you?")
+# print("{0} is a great answer.".format(answer))
+
+# list = [1,1,1,1,3,4,1,1,2,3]
+# list.reverse()
+# print(list)
+
+# str = "hello"
+# print(str.splice(0))
+
+# colors = 1,2,3
+# print(colors)
+
+# a = [('one', 1), ('two', 2)]
+# print(a)
+# newdict = dict(a)
+# print(newdict)
+# newdict(one) = 3
+# print(newdict)
+
+print(filter(, [1,2,3]))
