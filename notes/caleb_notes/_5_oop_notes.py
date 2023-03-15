a855f5eaf9c7bd8dea54ba0ea9d1841dfc8395e7
thedaebu <min.kim163@gmail.com>
2023-03-15 12:14:34 -0400

    3/15/23

--- notes/caleb_notes/_5_oop_notes.py
+++ notes/caleb_notes/_5_oop_notes.py
@@ -1,19 +1,61 @@
 ## object-oriented programming
 class Book():
     # class variable
-    favorites = [] 
+    # using class name for prefix refers to class
+    # using self for prefix refers to class instance
+    favorites = []
 
-    # all class method require first argument of self    
+    # all instance methods require first argument of self  
     def __init__(self, title, pages): # initializer/constructor method
         self.title = title
         self.pages = pages
 
-    # class method
     def is_long(self):
         if self.pages > 100:
             return True
         else:
             return False
+
+    # decorator for class method
+    # cls must be used as first argument to represent the class
+    @classmethod
+    def add_to_favorites(cls, book):
+        cls.favorites.append(book)
+
+    # class method can be used for alternate constructor method
+    @classmethod
+    def from_string(cls, book_str):
+        title, pages = book_str.split("-")
+        return cls(title, pages)
+
+    # denotes method as static method
+    @staticmethod
+    def is_long(pages):
+        return pages > 100
+
+    # accessor decorator allows defining through a method but can also be 
+    # accessed as an attribute
+    @property
+    def book_code(self):
+        return f'{self.title}-{self.pages}'
+
+    # setter decorator for property method
+    # use method_name.setter as decorator name
+    # use same method name for method name
+    @book_code.setter
+    def book_code(self, code):
+        title, pages = code.split("-")
+        self.title = title
+        self.pages = pages
+
+    @book_code.deleter
+    def book_code(self):
+        self.title = None
+        self.pages = None
+
+    # defines/overwrites return of class instantiation
+    def __repr__(self):
+        return f'{self.title} is {self.pages} long'
 
     # defines/overwrites str representation of an object in a class
     def __str__(self):
@@ -30,16 +72,28 @@ class Book():
     def __hash__(self):
         return hash(self.title) ^ hash(self.pages)
 
-book1 = Book('Second Wind', 102) # book object
-book2 = Book('Second Wind', 102) # book object
+# subclass has superclass as initial argument
+class ShortStory(Book):
+    def __init__(self, title, pages, genre):
+        super().__init__(title, pages)
+        self.genre = genre
 
-Book.favorites.extend([book1, book2])
-Book.favorites[0].title # => 'Second Wind'
-Book.favorites[0] # => 'Second Wind is 102 pages long.'
-book1 == book2 # => True # must use '==' and not 'is'
-hash(book1) == hash(book2) # => True
+# use one leading underscore when naming class variables
+# use __slots__ to set an array of class variables to be used for Python to work faster
+# inheritance
+	# use more general class as argument for more specific class
+	# in __init__ method, use the super().__init__ method
+# getter method
+	# use @property on the line before the getter method
+# setter method
+	# use @variableName.setter before the setter method
+book1 = ShortStory("The Book", 12, "Horror")
 
-def change_title(book): # passed by object reference method
-    book.title = "Third Wind"
+# returns if instance is instance of a class/superclass
+print(isinstance(book1, Book))
+print(isinstance(book1, ShortStory))
+# returns if class is subclass of a superclass
+print(issubclass(ShortStory, Book))
 
-change_title(book1) # book1.title changes to "Third Wind"
+# delete method call for class
+del book1.book_code
