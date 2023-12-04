934c2568252f434faf4f387b657fc422f88763c5
thedaebu <min.kim163@gmail.com>
2023-12-03 20:30:48 -0500

    change data used to queried data form database

--- django_practice/blog/views.py
+++ django_practice/blog/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
-# **
-from django.http import HttpResponse
+from .models import Post
+
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html')
