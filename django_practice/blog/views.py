8d298dc1b5032040982c151547e6e44fd36ff31b
thedaebu <min.kim163@gmail.com>
2023-12-04 15:39:08 -0500

    add create, update, and delete functionality for posts

--- django_practice/blog/views.py
+++ django_practice/blog/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
 from .models import Post
 
 
@@ -8,6 +16,57 @@ def home(request):
         'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html' # changes default html file url
+    context_object_name = 'posts' # changes default object variable name
+    ordering = ['-date_posted'] # sorts by model attribute
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+# LoginRequiredMixin requires being logged in or redirects
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    # overwrites form_valid method
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+# UserPassesTestMixin helps implement validations
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    # overwrites form_valid method
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    # used with UserPassesTestMixin
+    def test_func(self):
+        post = self.get_object() # gets the current model object
+        if self.request.user == post.author:
+            return True
+        else:
+            return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object() # gets the current model object
+        if self.request.user == post.author:
+            return True
+        else:
+            return False
 
 
 def about(request):
